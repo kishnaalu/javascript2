fa5d6415b6e00235b9433f173a91b22983a53f15
kishnaalu <kishnaalu78@gmail.com>
2024-05-08 11:55:09 +0545

    session

---
+++ index.js
@@ -0,0 +1,18 @@
+console.log("hello world");
+console.log("my name is sonu");
+
+let name1="sonu";
+let name2= name1;
+console.log(name2, "first")
+name1="suraj"
+console.log(name2,"second")
+
+const collegename="Butwal Kalika Campus"
+const batch=2022;
+const studentname="Sonu Thapa";
+const uniregno=760454;
+const faculty= "BCA"
+
+const a=2
+const b=3
+console.log(a ** b);
