9629643e1206e1fea483f44aaff41a60a24e3e35
kishnaalu <kishnaalu78@gmail.com>
2024-05-08 12:30:21 +0545

    todays work

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-console.log("hello world");
-console.log("my name is sonu");
+// console.log("hello world");
+// console.log("my name is sonu");
 
-let name1="sonu";
-let name2= name1;
-console.log(name2, "first")
-name1="suraj"
-console.log(name2,"second")
+// let name1="sonu";
+// let name2= name1;
+// console.log(name2, "first")
+// name1="suraj"
+// console.log(name2,"second")
 
 const collegename="Butwal Kalika Campus"
 const batch=2022;
 const studentname="Sonu Thapa";
 const uniregno=760454;
 const faculty= "BCA"
+const location="Butwal"
+console.log(`My name is ${studentname}. My faculty is ${faculty} in ${collegename}. It is located in ${location}.`)
 
-const a=2
-const b=3
-console.log(a ** b);
+let token="access_token=ghjghfjhfjh"
+let key=token.split("=")[0]
+console.log(key)
+console.log(token.split("=")[1]);
+
