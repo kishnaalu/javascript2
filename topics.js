ff4b4ee1fa2c4ac44749a427a2f8518cb9783d2f
kishnaalu <kishnaalu78@gmail.com>
2024-05-09 11:19:31 +0545

    Topic we coveredd

---
+++ topics.js
@@ -0,0 +1,84 @@
+/* 
+
+Day 1:
+->console.log()
+->export to github: create account on github
+                    create repo on github
+                    create folder on desktop and open in vs code
+                    cntrl+back-tick to open terminal and type git config -global user.name "github_username" and enter
+                    git config -global user.email "github_email" and enter
+                    copy code line by line from new repo created in github to push readme file and export your file 
+
+Day 2:
+Download node js
+check version from vs by node --version
+open nodemon in google and copy code to install npm
+type code and enter type node file_name to run the program
+and can also do nodemon file_name to run file after save(cntrl+s) 
+
+                    
+Day 3: (For API)
+->down extension thunder client 
+  search dummy json on google
+  copy url and paste in new request
+https://hoppscotch.io/ another link for API 
+KNEW ABOUT GET POST PATCH
+
+
+Day 4:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
